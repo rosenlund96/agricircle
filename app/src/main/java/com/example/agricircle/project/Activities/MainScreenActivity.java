85465a29fb672a4516ed306da48f139600e7bc79
mrla2 <mrla@netcompany.com>
2019-11-18 11:13:54 +0100

    Changes to mainscreenactivity. All calls to graphql not handled by asynctask, to smoothen out things.

--- app/src/main/java/com/example/agricircle/project/Activities/MainScreenActivity.java
+++ app/src/main/java/com/example/agricircle/project/Activities/MainScreenActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
@@ -27,6 +28,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.PolygonOptions;
 
@@ -74,12 +76,10 @@ public class MainScreenActivity extends AppCompatActivity
         mPrefs = getPreferences(MODE_PRIVATE);
         currentState = STATE.GETCOOKIE;
         progressDialog = new ProgressDialog(MainScreenActivity.this,
-                R.style.Theme_AppCompat_Dialog);
-        progressDialog.setIndeterminate(true);
+                R.style.AppCompatAlertDialogStyle);
 
-        progressDialog.show();
-        loadAssets();
-
+        //loadAssets();
+        new LoadAssetsAsync().execute();
 
 
 
@@ -100,6 +100,125 @@ public class MainScreenActivity extends AppCompatActivity
     public static MainScreenActivity getInstance() {
         return sMainScreenActivity;
     }
+
+             private class LoadAssetsAsync extends AsyncTask<Void, Void, Void>{
+
+                 @Override
+                 protected void onPostExecute(Void aVoid) {
+                     super.onPostExecute(aVoid);
+                     progressDialog.dismiss();
+
+                     fragmentManager.beginTransaction()
+                             //.addToBackStack(null)
+                             .replace(R.id.article_fragment
+                                     , new MapFragment(),"MAP")
+
+                             .commit();
+                 }
+
+                 @Override
+                 protected Void doInBackground(Void... voids) {
+                     while(true){
+                         switch(currentState){
+                             case GETCOOKIE:
+                                 cookie = i.getStringExtra("Cookie");
+                                 progressDialog.setMessage("Henter Cookie");
+                                 while(true){
+                                     if(cookie != null){
+                                         controller = new UserController(cookie);
+                                         currentState = STATE.LOADUSER;
+                                         System.out.println("Cookie hentet");
+                                         break;
+                                     }
+                                 }
+
+                             case LOADUSER:
+                                 user = (User) i.getSerializableExtra("User");
+                                 progressDialog.setMessage("Henter Bruger");
+                                 while(true){
+                                     if(user != null){
+                                         controller.setUser(user);
+                                         //controller.getCompanies();
+                                         setUserSettings();
+                                         //System.out.println("Bruger hentet");
+                                         currentState = STATE.LOADCOMPANIES;
+                                         break;
+                                     }
+                                 }
+                             case LOADCOMPANIES:
+                                 Boolean getCompaniesCalled = false;
+                                 progressDialog.setMessage("Henter Virksomheder");
+
+                                 while(true){
+                                     if(controller.getUser().getCompanies().size() > 0){
+
+                                         System.out.println("Virksomheder hentet");
+                                         currentState = STATE.LOADCROPS;
+                                         break;
+                                     }
+                                     else if(controller.getUser().getCompanies().isEmpty() && !getCompaniesCalled){
+                                         controller.getCompanies();
+                                         getCompaniesCalled = true;
+                                     }
+                                 }
+
+
+                             case LOADCROPS:
+
+                                 for(int i = 0; i<controller.getUser().getCompanies().size();i++){
+                                     System.out.println("Virksomhed "+(i+1)+" "+controller.getUser().getCompanies().get(i).getName());
+                                 }
+                                 System.out.println("Henter crops for: " +controller.getUser().getCompanies().get(0).getName());
+                                 //controller.getCrops(controller.getUser().getCompanies().get(0).getId(),2019);
+                                 controller.getCrops(controller.user.getPrimaryCompany(),2019);
+                                 progressDialog.setMessage("Henter Crops");
+                                 while(true){
+                                     if(controller.cropsList.size() > 0){
+                                         System.out.println("Crops hentet: " + controller.cropsList.size());
+                                         currentState = STATE.LOADFIELDS;
+                                         break;
+                                     }
+                                 }
+
+                             case LOADFIELDS:
+                                 progressDialog.setMessage("Henter Fields");
+                                 controller.getFields();
+                                 while(true){
+
+                                     if(controller.getFieldsLoaded()){
+                                         System.out.println("Fields hentet" + "Antal: " + controller.getUser().getFields().size());
+                                         currentState = STATE.SETOBJECTS;
+                                         break;
+                                     }
+                                 }
+
+
+                             case SETOBJECTS:
+                                 controller.createDummyData(controller.getUser().getFields(),controller.getCropsList());
+
+                                 break;
+
+
+
+                         }
+
+                         break;
+                     }
+
+
+
+
+                     return null;
+                 }
+
+                 @Override
+                 protected void onPreExecute() {
+                     super.onPreExecute();
+                     //progressDialog.setIndeterminate(true);
+                     progressDialog.setMessage("Henter informationer fra server");
+                     progressDialog.show();
+                 }
+             }
 
     public void loadAssets(){
 
@@ -235,48 +354,6 @@ public class MainScreenActivity extends AppCompatActivity
                  return result;
 
              }
-
-
-
-
-
-
-
-
-
-
-
-
-    public void ShowPopup(View v) {
-
-        Button knap;
-        myDialog.setContentView(R.layout.popup_modal);
-        knap = (Button) myDialog.findViewById(R.id.alert_btn_okay);
-        knap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myDialog.dismiss();
-            }
-        });
-        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-        myDialog.getWindow().setLayout((int)(width ),(int)(height * 0.89 ));
-        myDialog.show();
-    }
-    public PolygonOptions DrawPolygon(Field field){
-        PolygonOptions rectOptions = new PolygonOptions();
-        for(int i = 0; i<field.getCoordinates().getNumOfCoordinates(); i++){
-            rectOptions.add(field.getCoordinates().getCoordinates().get(i));
-
-        }
-        rectOptions.add(field.getCoordinates().getCoordinates().get(0));
-        rectOptions.strokeColor(Color.RED);
-        rectOptions.fillColor(Color.GREEN);
-        return rectOptions;
-    }
 
 
 
